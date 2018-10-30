4f77089c43122d6424cf4e6e4128b92f11dcb8f2
Smith <DDSmith@reedtech.com>
2018-10-30 08:20:46 -0400

    Add project files.

---
+++ GitConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GitConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Git Hub!");
+            Console.ReadLine();
+        }
+    }
+}
